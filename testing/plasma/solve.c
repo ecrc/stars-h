69d947deb94db9d6189095bca052f7eee2d2471e
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2017-03-13 18:53:20 +0300

    Changes to CMakeLists.txt and test for plasma

--- testing/plasma/solve.c
+++ testing/plasma/solve.c
@@ -9,14 +9,14 @@
 
 int main(int argc, char **argv)
 {
-    if(argc < 4)
+    if(argc < 3)
     {
         printf("%d\n", argc);
-        printf("spatial n block_size beta\n");
+        printf("spatial n block_size\n");
         exit(1);
     }
     int n = atoi(argv[1]), block_size = atoi(argv[2]);
-    double beta = atof(argv[3]);
+    double beta = 0.1;
     int maxrank = 100, oversample = 10, onfly = 0;
     double tol = 1e-9;
     char *scheme = "omp_rsdd";
@@ -35,26 +35,21 @@ int main(int argc, char **argv)
     // Get array from problem
     Array *A;
     double time0 = omp_get_wtime();
-    array_from_buffer(&A, 2, shape, 'd', 'F', NULL);
+    //array_from_buffer(&A, 2, shape, 'd', 'F', NULL);
     starsh_problem_to_array(P, &A);
     time0 = omp_get_wtime()-time0;
     printf("Compute entire matrix: %e secs\n", time0);
     int N = A->shape[0];
-    int nrhs = 10;
-    double *b, *x, *x_CG, *CG_work;
+    int nrhs = 1;
+    double *b, *b_CG, *x, *x_CG, *CG_work;
     STARSH_MALLOC(b, N*nrhs);
+    STARSH_MALLOC(b_CG, N*nrhs);
     STARSH_MALLOC(x, N*nrhs);
     STARSH_MALLOC(x_CG, N*nrhs);
     int iseed[4] = {0, 0, 0, 1};
     LAPACKE_dlarnv_work(3, iseed, N*nrhs, b);
     //LAPACKE_dlarnv_work(3, iseed, N, x);
     STARSH_MALLOC(CG_work, 3*(N+1)*nrhs);
-    plasma_init();
-    time0 = omp_get_wtime();
-    solve(N, A->data, N, nrhs, b, N, x, N);
-    time0 = omp_get_wtime()-time0;
-    printf("Time to solve SPD problem: %e secs\n", time0);
-    plasma_finalize();
     // Init tiled cluster for tiled low-rank approximation and print info
     time0 = omp_get_wtime();
     STARSH_cluster *C;
@@ -74,16 +69,25 @@ int main(int argc, char **argv)
     printf("TIME TO APPROXIMATE: %e secs\n", time1);
     // Solve with CG, approximate solution is in x, initial guess is zero
     //memset(x, 0, data->count*sizeof(double));
-    cblas_dcopy(N, b, 1, x_CG, 1);
+    cblas_dcopy(N*nrhs, b, 1, x_CG, 1);
     time1 = omp_get_wtime();
     int info = starsh_itersolvers__dcg(M, nrhs, b, N, x_CG, N, tol, CG_work);
     printf("CG INFO: %d\n", info);
-    cblas_daxpy(N*nrhs, -1.0, x, 1, x_CG, 1);
     //for(int i = 0; i < 1000; i++)
     //    starsh_blrm__dmml_omp(M, 1, 1.0, b, N, 0.0, x, N);
     double time2 = omp_get_wtime();
     printf("TIME TO SOLVE: %e secs\n", time2-time1);
     printf("TOTAL TIME FOR STARSH: %e secs\n", time2-time0);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, N, nrhs, N, 1.0, A->data, N, x_CG, N, 0.0, b_CG, N);
+    plasma_init();
+    time0 = omp_get_wtime();
+    solve(N, A->data, N, nrhs, b, N, x, N);
+    time0 = omp_get_wtime()-time0;
+    printf("TIME PLASMA: %e secs\n", time0);
+    plasma_finalize();
+    cblas_daxpy(N*nrhs, -1.0, x, 1, x_CG, 1);
+    cblas_daxpy(N*nrhs, -1.0, b, 1, b_CG, 1);
     printf("ACCURACY OF SOLUTION: %e\n", cblas_dnrm2(N*nrhs, x_CG, 1)/cblas_dnrm2(N*nrhs, x, 1));
+    printf("ACCURACY OF RHS: %e\n", cblas_dnrm2(N*nrhs, b_CG, 1)/cblas_dnrm2(N*nrhs, b, 1));
     return 0;
 }
