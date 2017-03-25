4858981d2dc03056b1a5d627cd0d2da6f71cb30c
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2017-03-25 10:13:56 +0300

    Fixing uninitialized initial solution

--- testing/mpi.c
+++ testing/mpi.c
@@ -77,33 +77,32 @@ int main(int argc, char **argv)
                 time1, rel_err);
     // Flush STDOUT, since next step is very time consuming
     fflush(stdout);
-    // Multiply TLR matrix by vector
-    double *b, *b_CG, *x, *x_CG, *CG_work;
+    // Solve with CG, initial solution x=0
+    // b is RHS and r is residual
+    double *b, *x, *r, *CG_work;
     int nrhs = 1;
     STARSH_MALLOC(b, N*nrhs);
-    STARSH_MALLOC(b_CG, N*nrhs);
     STARSH_MALLOC(x, N*nrhs);
-    STARSH_MALLOC(x_CG, N*nrhs);
+    STARSH_MALLOC(r, N*nrhs);
     STARSH_MALLOC(CG_work, 3*(N+1)*nrhs);
     if(mpi_rank == 0)
     {
         int iseed[4] = {0, 0, 0, 1};
         LAPACKE_dlarnv_work(3, iseed, N*nrhs, b);
+        cblas_dscal(N*nrhs, 0.0, x, 1);
     }
-    starsh_blrm__dmml_mpi(M, nrhs, 1.0, b, N, 0.0, x, N);
     MPI_Barrier(MPI_COMM_WORLD);
     time1 = MPI_Wtime();
-    int info = starsh_itersolvers__dcg_mpi(M, nrhs, b, N, x_CG, N, tol,
-            CG_work);
+    int info = starsh_itersolvers__dcg_mpi(M, nrhs, b, N, x, N, tol, CG_work);
     time1 = MPI_Wtime()-time1;
-    starsh_blrm__dmml_mpi(M, nrhs, 1.0, x_CG, N, 0.0, b_CG, N);
+    starsh_blrm__dmml_mpi(M, nrhs, -1.0, x, N, 0.0, r, N);
     if(mpi_rank == 0)
     {
-        cblas_daxpy(N, -1.0, b, 1, b_CG, 1);
-        double norm = cblas_dnrm2(N, b, 1);
-        double diff = cblas_dnrm2(N, b_CG, 1);
+        cblas_daxpy(N, 1.0, b, 1, r, 1);
+        double norm_rhs = cblas_dnrm2(N, b, 1);
+        double norm_res = cblas_dnrm2(N, r, 1);
         printf("CG INFO=%d\nCG TIME=%f secs\nCG RELATIVE ERROR IN RHS=%e\n",
-                info, time1, diff/norm);
+                info, time1, norm_res/norm_rhs);
     }
     MPI_Finalize();
     return 0;
