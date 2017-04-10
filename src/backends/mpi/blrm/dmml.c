a923d5242e326ad57124820aadeaa732a564bf37
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2017-04-10 12:27:26 +0300

    Fixed problem when number of tiles in one dimension was not dividable by grid of processes in one dimension

--- src/backends/mpi/blrm/dmml.c
+++ src/backends/mpi/blrm/dmml.c
@@ -215,11 +215,13 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
     for(int i = 0; i < grid_ny; i++)
         group_rank[i] = i;
     MPI_Group_incl(mpi_world_group, grid_ny, group_rank, &mpi_leadingy_group);
-    MPI_Comm_create_group(MPI_COMM_WORLD, mpi_leadingy_group, 0, &mpi_leadingy);
+    MPI_Comm_create_group(MPI_COMM_WORLD, mpi_leadingy_group, 0,
+            &mpi_leadingy);
     for(int i = 0; i < grid_nx; i++)
         group_rank[i] = i*grid_ny;
     MPI_Group_incl(mpi_world_group, grid_nx, group_rank, &mpi_leadingx_group);
-    MPI_Comm_create_group(MPI_COMM_WORLD, mpi_leadingx_group, 0, &mpi_leadingx);
+    MPI_Comm_create_group(MPI_COMM_WORLD, mpi_leadingx_group, 0,
+            &mpi_leadingx);
     MPI_Comm_split(MPI_COMM_WORLD, grid_x, mpi_rank, &mpi_splitx);
     MPI_Comm_split(MPI_COMM_WORLD, grid_y, mpi_rank, &mpi_splity);
     int mpi_leadingx_rank=-1, mpi_leadingx_size=-1;
@@ -248,10 +250,11 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
             mpi_splity_size);
     */
     int grid_block_size = maxnb*grid_nx;
-    int ld_temp_A = ncols/grid_nx;
+    int ld_temp_A = (F->nbcols+grid_nx-1-grid_x)/grid_nx*maxnb;
     double *temp_A;
     STARSH_MALLOC(temp_A, nrhs*ld_temp_A);
     if(mpi_leadingx != MPI_COMM_NULL)
+    {
         for(int i = 0; i < F->nbcols/grid_nx; i++)
         {
             double *src = A+i*grid_block_size;
@@ -262,7 +265,42 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
                         maxnb, MPI_DOUBLE, 0, mpi_leadingx);
             }
         }
+        int i = F->nbcols/grid_nx;
+        int remain = F->nbcols-i*grid_nx;
+        if(remain > 0)
+        {
+            double *src = A+i*grid_block_size;
+            double *recv = temp_A+i*maxnb;
+            if(mpi_rank == 0)
+            {
+                int sendcounts[grid_nx], displs[grid_nx];
+                for(int j = 0; j < remain; j++)
+                    sendcounts[j] = maxnb;
+                for(int j = remain; j < grid_nx; j++)
+                    sendcounts[j] = 0;
+                displs[0] = 0;
+                for(int j = 1; j < grid_nx; j++)
+                    displs[j] = displs[j-1]+sendcounts[j-1];
+                for(int j = 0; j < nrhs; j++)
+                    MPI_Scatterv(src+j*lda, sendcounts, displs, MPI_DOUBLE,
+                            recv+j*ld_temp_A, maxnb, MPI_DOUBLE, 0,
+                            mpi_leadingx);
+            }
+            else
+            {
+                int recvcount = 0;
+                if(grid_x < remain)
+                    recvcount = maxnb;
+                for(int j = 0; j < nrhs; j++)
+                    MPI_Scatterv(NULL, NULL, NULL, MPI_DOUBLE,
+                            recv+j*ld_temp_A, recvcount, MPI_DOUBLE, 0,
+                            mpi_leadingx);
+            }
+        }
+    }
     MPI_Bcast(temp_A, nrhs*ld_temp_A, MPI_DOUBLE, 0, mpi_splitx);
+    //if(mpi_rank == 0)
+    //    STARSH_WARNING("DATA DISTRIBUTED!");
     //for(int i = 0; i < nrhs; i++)
     //    MPI_Bcast(A+i*lda, ncols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     double *temp_D, *temp_B;
@@ -278,7 +316,8 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
     {
         STARSH_MALLOC(temp_D, num_threads*maxnb*maxnb);
     }
-    int ldout = nrows/grid_ny;
+    int ldout = (F->nbrows+grid_ny-1-grid_y)/grid_ny*maxnb;
+    //STARSH_WARNING("MPI=%d ldA=%d ldB=%d", mpi_rank, ld_temp_A, ldout);
     STARSH_MALLOC(temp_B, num_threads*nrhs*ldout);
     // Setting temp_B=beta*B for master thread of root node and B=0 otherwise
     #pragma omp parallel
@@ -302,6 +341,8 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
             for(int j = 0; j < nrhs; j++)
                 temp_B[j*ldb+i] *= beta;
     }
+    //if(mpi_rank == 0)
+    //    STARSH_WARNING("MORE DATA DISTRIBUTED");
     // Simple cycle over all far-field admissible blocks
     #pragma omp parallel for schedule(dynamic, 1)
     for(lbi = 0; lbi < nblocks_far_local; lbi++)
@@ -326,8 +367,10 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
                 ncols, 1.0, V, ncols, temp_A+(j/grid_nx)*maxnb, ld_temp_A, 0.0,
                 D, rank);
         cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, nrows, nrhs,
-                rank, alpha, U, nrows, D, rank, 1.0, out+i/grid_ny*maxnb, ldout);
+                rank, alpha, U, nrows, D, rank, 1.0, out+i/grid_ny*maxnb,
+                ldout);
     }
+    //STARSH_WARNING("NODE %d DONE WITH FAR", mpi_rank);
     if(M->onfly == 1)
         // Simple cycle over all near-field blocks
         #pragma omp parallel for schedule(dynamic, 1)
@@ -381,6 +424,8 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
         for(int j = 0; j < nrhs; j++)
             for(int k = 1; k < num_threads; k++)
                 temp_B[j*ldout+i] += temp_B[(k*nrhs+j)*ldout+i];
+    //STARSH_WARNING("NODE %d DONE WITH OMP REDUCTION", mpi_rank);
+    MPI_Barrier(MPI_COMM_WORLD);
     // Since I keep result only on root node, following code is commented
     //for(int i = 0; i < nrhs; i++)
     //    MPI_Allreduce(temp_B+i*ldout, B+i*ldb, ldout, MPI_DOUBLE, MPI_SUM,
@@ -410,6 +455,36 @@ int starsh_blrm__dmml_mpi_tiled(STARSH_blrm *M, int nrhs, double alpha,
             for(int j = 0; j < nrhs; j++)
                 MPI_Gather(src+j*ldout, maxnb, MPI_DOUBLE, recv+j*ldb, maxnb,
                         MPI_DOUBLE, 0, mpi_leadingy);
+        }
+        int i = F->nbrows/grid_ny;
+        int remain = F->nbrows-i*grid_ny;
+        if(remain > 0)
+        {
+            double *src = final_B+i*maxnb;
+            double *recv = B+i*maxnb*grid_ny;
+            if(mpi_rank == 0)
+            {
+                int recvcounts[grid_ny], displs[grid_ny];
+                for(int j = 0; j < remain; j++)
+                    recvcounts[j] = maxnb;
+                for(int j = remain; j < grid_ny; j++)
+                    recvcounts[j] = 0;
+                displs[0] = 0;
+                for(int j = 1; j < grid_ny; j++)
+                    displs[j] = displs[j-1]+recvcounts[j-1];
+                for(int j = 0; j < nrhs; j++)
+                    MPI_Gatherv(src+j*ldout, maxnb, MPI_DOUBLE, recv+j*ldb,
+                            recvcounts, displs, MPI_DOUBLE, 0, mpi_leadingy);
+            }
+            else
+            {
+                int sendcount = 0;
+                if(grid_y < remain)
+                    sendcount = maxnb;
+                for(int j = 0; j < nrhs; j++)
+                    MPI_Gatherv(src+j*ldout, sendcount, MPI_DOUBLE, NULL, NULL,
+                            NULL, MPI_DOUBLE, 0, mpi_leadingy);
+            }
         }
     }
     free(temp_B);
