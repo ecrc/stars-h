7cc8e7e0c4b8371b80417e556a30c345fae0804b
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2017-05-10 12:48:48 +0300

    New examples for building approximation and fix for omp_* schemes, not updating memory footprint

--- examples/approximation/minimal.c
+++ examples/approximation/minimal.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "starsh.h"
+#include "starsh-minimal.h"
 
 int main(int argc, char **argv)
 {
-    // Matrix size
+    int problem_ndim = 2;
+    // Since there is only one kernel for minimal, kernel_type is ignored
+    int kernel_type = 0;
+    // Size of desired matrix
     int N = 2500;
-    int shape[2] = {N, N}; // shape of corresponding array
-    char dtype = 'd'; // 'd' for double precision array
-    char order = 'F'; // Fortran order (column-major)
-    char symm = 'N'; // 'S' for symmetric problem, 'N' for nonsymmetric
+    // 'N' for nonsymmetric matrix and 'd' for double precision
+    char symm = 'N', dtype = 'd';
+    int ndim = 2, shape[2] = {N, N};
     int info;
     int maxrank = 30; // Maximum rank to be used
     int oversample = 10; // Parameter for randomized SVD (extra random vectors)
@@ -19,43 +22,34 @@ int main(int argc, char **argv)
                                      // compression
     // Size of tile
     int block_size = 250;
-    // Allocate memory for dense matrix
-    double *data = malloc(N*N*sizeof(*data));
-    if(data == NULL)
-    {
-        printf("Error in allocation of memory for data buffer\n");
-        exit(1);
-    }
-    // Iterate over columns
-    for(int j = 0; j < N; j++)
-        // Iterate over rows
-        for(int i = 0; i < N; i++)
-        {
-            // Fill matrix in Fortran order (column-major)
-            data[j*N+i] = 1.0;
-            // Make this matrix diagonally dominant
-            if(i == j)
-                data[j*N+i] += N;
-        }
-    // Create structure array to cover double *data
-    Array *array;
-    info = array_from_buffer(&array, 2, shape, dtype, order, data);
+    srand(0);
+    // Generate data for spatial statistics problem
+    STARSH_mindata *data;
+    STARSH_kernel kernel;
+    // STARSH_MINIMAL for random tile low-rank matrix
+    // minimal problem does not have any parameters
+    // 0 at the end to indicate end of arguments
+    info = starsh_application((void **)&data, &kernel, N, dtype,
+            STARSH_MINIMAL, kernel_type, 0);
     if(info != 0)
     {
-        printf("Error in creation of Array structure\n");
-        exit(info);
+        printf("wrong parameters for minimal example\n");
+        return info;
     }
-    // Set up problem
+    // Init problem with given data and kernel and print short info
     STARSH_problem *problem;
-    info = starsh_problem_from_array(&problem, array, symm);
+    info = starsh_problem_new(&problem, ndim, shape, symm, dtype, data, data,
+            kernel, "Minimal example");
     if(info != 0)
     {
-        printf("Error in creation of problem\n");
+        printf("Error in STARSH problem\n");
         exit(info);
     }
+    printf("STARSH problem was succesfully generated\n");
+    starsh_problem_info(problem);
     // Set up clusterization (divide rows and columns into blocks)
     STARSH_cluster *cluster;
-    info = starsh_cluster_new_tiled(&cluster, array, N, block_size);
+    info = starsh_cluster_new_tiled(&cluster, data, N, block_size);
     if(info != 0)
     {
         printf("Error in creation of cluster\n");
---
+++ examples/approximation/rndtiled.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "starsh.h"
+#include "starsh-rndtiled.h"
+
+int main(int argc, char **argv)
+{
+    int problem_ndim = 2;
+    // Since there is only one kernel for rndtiled, kernel_type is ignored
+    int kernel_type = 0;
+    // Size of desired matrix
+    int N = 2500;
+    // Size of tile for tile low-rank random matrix
+    int block_size = 250;
+    // Singular values of each tile are 1, decay, decay^2, ...
+    double decay = 0.5;
+    // Addition for diagonal elements
+    double diag = N;
+    // 'N' for nonsymmetric matrix and 'd' for double precision
+    char symm = 'N', dtype = 'd';
+    int ndim = 2, shape[2] = {N, N};
+    int info;
+    int maxrank = 30; // Maximum rank to be used
+    int oversample = 10; // Parameter for randomized SVD (extra random vectors)
+    double tol = 1e-3; // Error threshold
+    int onfly = 0; // Do not store dense blocks (since they are stored in data)
+    char *scheme = "omp_rsdd"; // Use OpenMP randomized SVD for
+                                     // compression
+    srand(0);
+    // Generate data for spatial statistics problem
+    STARSH_rndtiled *data;
+    STARSH_kernel kernel;
+    // STARSH_RNDTILED for random tile low-rank matrix
+    // STARSH_RNDTILED_NB to indicate next parameter shows size of tile
+    // STARSH_RNDTILED_DECAY to indicate next parameter is decay of singular
+    //   values
+    // STARSH_RNDTILED_DIAG to indicate next parameter is addition for diagonal
+    //   elements
+    // 0 at the end to indicate end of arguments
+    info = starsh_application((void **)&data, &kernel, N, dtype,
+            STARSH_RNDTILED, kernel_type, STARSH_RNDTILED_NB, block_size,
+            STARSH_RNDTILED_DECAY, decay, STARSH_RNDTILED_DIAG, diag, 0);
+    if(info != 0)
+    {
+        printf("wrong parameters for random tile low-rank matrix\n");
+        return info;
+    }
+    // Init problem with given data and kernel and print short info
+    STARSH_problem *problem;
+    info = starsh_problem_new(&problem, ndim, shape, symm, dtype, data, data,
+            kernel, "Random tile low-rank matrix");
+    if(info != 0)
+    {
+        printf("Error in STARSH problem\n");
+        exit(info);
+    }
+    printf("STARSH problem was succesfully generated\n");
+    starsh_problem_info(problem);
+    // Set up clusterization (divide rows and columns into blocks)
+    STARSH_cluster *cluster;
+    info = starsh_cluster_new_tiled(&cluster, data, N, block_size);
+    if(info != 0)
+    {
+        printf("Error in creation of cluster\n");
+        exit(info);
+    }
+    // Set up format (divide matrix into tiles)
+    STARSH_blrf *format;
+    info = starsh_blrf_new_tiled(&format, problem, cluster, cluster, symm);
+    if(info != 0)
+    {
+        printf("Error in creation of format\n");
+        exit(info);
+    }
+    // Approximate with tile low-rank matrix
+    STARSH_blrm *matrix;
+    info = starsh_blrm_approximate(&matrix, format, maxrank, oversample, tol,
+            onfly, scheme);
+    if(info != 0)
+    {
+        printf("Error in approximation\n");
+        exit(info);
+    }
+    printf("Done with approximation!\n");
+    starsh_blrf_info(format); // Show info about format
+    starsh_blrm_info(matrix); // Show info about approximation
+    // Show realtive error of approximation in Frobenius norm
+    double rel_err = starsh_blrm__dfe(matrix);
+    printf("Relative error in Frobenius norm: %e\n", rel_err);
+    return 0;
+}
