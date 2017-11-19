e6f3350613e4bfcd7d99c5398f9d26eeae34a631
Eduardo Gonzalez Fisher <eduardo.gonzalezfisher@kaust.edu.sa>
2017-11-19 14:27:59 +0300

    jenkinsfile: change into new modules

--- jenkins-scripts/base-environment.sh
+++ jenkins-scripts/base-environment.sh
@@ -1,12 +1,12 @@
 # This is meant to be sourced from a sh script
 
 # loads modules
-module load gcc/5.3.0
+module load gcc/5.5.0
 module load mkl/2018-initial
-module load mpi-openmpi/1.10.2-gcc-5.3.0
-module load starpu/1.2.1-gcc-5.3.0
-module load gsl/2.3-gcc-5.3.0
-module load cmake/3.3.2
+module load openmpi/3.0.0-gcc-5.5.0
+module load starpu/1.2.3-gcc-5.5.0-mkl-openmpi-3.0.0
+module load gsl/2.4-gcc-5.5.0
+module load cmake/3.9.6
 
 # variables
 BUILDDIR="$WORKSPACE/build/"
